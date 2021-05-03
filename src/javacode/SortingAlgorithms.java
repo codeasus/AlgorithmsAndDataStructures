14078f10568e05db5c9e54add07dd3900f076a51
Orhan Rosenfeld <izhkq9@inf.elte.hu>
2021-05-03 19:45:03 +0400

    Add sorting algorthms implementations

---
+++ src/javacode/SortingAlgorithms.java
@@ -0,0 +1,18 @@
+package javacode;
+
+public class SortingAlgorithms {
+    
+    private SortingAlgorithms(){}
+
+    public static void bubbleSortBasic(int[] inputArray) {
+        for(int i = 0; i < inputArray.length; i++) {
+            for (int j = i + 1; j < inputArray.length ; j++) {
+                if(inputArray[i] > inputArray[j]) {
+                    int tempValue = inputArray[j];
+                    inputArray[j] = inputArray[i];
+                    inputArray[i] = tempValue;
+                }
+            }
+        }
+    }
+}
