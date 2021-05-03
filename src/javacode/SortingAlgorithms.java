4eca8402dea69ad033bf3d3f00a368dd82b2ff5b
Orhan Rosenfeld <izhkq9@inf.elte.hu>
2021-05-03 19:46:19 +0400

    Fix empty space

--- src/javacode/SortingAlgorithms.java
+++ src/javacode/SortingAlgorithms.java
@@ -6,7 +6,7 @@ public class SortingAlgorithms {
 
     public static void bubbleSortBasic(int[] inputArray) {
         for(int i = 0; i < inputArray.length; i++) {
-            for (int j = i + 1; j < inputArray.length ; j++) {
+            for (int j = i + 1; j < inputArray.length; j++) {
                 if(inputArray[i] > inputArray[j]) {
                     int tempValue = inputArray[j];
                     inputArray[j] = inputArray[i];
