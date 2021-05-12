05b97874822af3d59134dec4570f84f2e285c37e
Orhan Rosenfeld <izhkq9@inf.elte.hu>
2021-05-13 02:23:03 +0400

    Implemented SelectionSort

--- src/javacode/SortingAlgorithms.java
+++ src/javacode/SortingAlgorithms.java
@@ -6,8 +6,8 @@ public class SortingAlgorithms {
 
     public static void bubbleSortBasic(int[] inputArray) {
         for(int  loopIndex = 0; loopIndex < inputArray.length; loopIndex++){
-            for(int index = 0; index < inputArray.length - 1; index ++) {
-                if(inputArray[index] > inputArray[index + 1]) {
+            for(int index  = 0; index < inputArray.length - 1; index ++) {
+                if(inputArray[index]  > inputArray[index + 1]) {
                     int tempVariable      = inputArray[index];
                     inputArray[index]     = inputArray[index + 1];
                     inputArray[index + 1] = tempVariable;
@@ -18,7 +18,6 @@ public class SortingAlgorithms {
 
     public static void bubbleSort(int[] inputArray) {
         boolean sorted    = false;
-
         while(!sorted) {
             sorted        = true;
             for(int index = 0; index < inputArray.length - 1; index++) {
@@ -32,9 +31,29 @@ public class SortingAlgorithms {
         }
     }
 
-    public static void selectionSortBasic(int[] inputArray) {
-        
+    public static void selectionSort(int[] inputArray) {
+        for(int index = 0; index < inputArray.length; index++) {
+            for(int index_sec = index + 1; index_sec < inputArray.length; index_sec++) {
+                if(inputArray[index] > inputArray[index_sec]) {
+                    int              temp = inputArray[index];
+                    inputArray[index]     = inputArray[index_sec];
+                    inputArray[index_sec] = temp;
+                }
+            }
+        }
     }
 
-    public static void selectionSortRecursive(int elementToLoop, int[] inputArray) {};
+    public static void selectionSortRecursive(int elementToLoop, int[] inputArray) {
+        if(elementToLoop == 1) {
+            return;
+        }
+        for(int index = elementToLoop - 1; index >= 0; index--) {
+            if (inputArray[elementToLoop] < inputArray[index]) {
+                int                  temp = inputArray[elementToLoop];
+                inputArray[elementToLoop] = inputArray[index];
+                inputArray[index]         = temp;
+            }
+        }
+        selectionSortRecursive(elementToLoop - 1, inputArray);
+    }
 }
