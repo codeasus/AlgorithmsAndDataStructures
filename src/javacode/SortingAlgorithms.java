f9323506b75c7ca6c776cf2debb50efd61f02cb6
Orhan Rosenfeld <izhkq9@inf.elte.hu>
2021-05-17 00:29:46 +0400

    Reorganize the file

--- src/javacode/SortingAlgorithms.java
+++ src/javacode/SortingAlgorithms.java
@@ -87,29 +87,4 @@ public class SortingAlgorithms {
 
         return (elementPointer + 1);
     }
-
-    //  [9, 4, 12, 304, 12, 5, 102, 130, 1]
-    //  [0]  ==> [9, 4, 5, 1] : [12, 12, 304, 102, 130]
-    //  [1L] ==> [9, 4, 5, 1] > [4, 1] : [5, 9] > [1] : [4] : [5] : [9]
-    //  [1R] ==> [12, 12, 304, 102, 130] > [12, 12, 102, 130], [304] > [12, 12, 102] : [130] > [] : [12, 12, 102]
-
-    public static void quickSort_basic(int[] array) {
-        
-        int pivot         = (int) (array.length / 2);
-        int leftPointer   = 0;
-        int rightPointer  = array.length - 1;
-        
-        int tracer        = 0;
-        
-        for(int index = 0; index < array.length; index++) {
-            if(array[tracer] < array[pivot]) {
-                tracer++;
-                
-                
-                
-            }
-        }
-        
-
-    }
 }
