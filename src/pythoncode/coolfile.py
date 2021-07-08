d9634420b7c4b554adab66d54b38a64bac9d401f
Orhan Rosenfeld <izhkq9@inf.elte.hu>
2021-07-08 14:38:20 +0400

    Add \'coolfile\' python module for operations on files/paths

---
+++ src/pythoncode/coolfile.py
@@ -0,0 +1,16 @@
+import os
+
+def displayPath(current_path, intend=' '):
+    if os.path.isfile(current_path): 
+        print(current_path)
+
+    if os.path.isdir(current_path):
+        for file_ in os.listdir(current_path): 
+            if os.path.isfile('{}\\{}'.format(current_path, file_)): 
+                print('{0}{1}\\{2}'.format(intend*2, current_path, file_))
+            if os.path.isdir('{}\\{}'.format(current_path, file_)): 
+                print('{0}{1}\\{2}'.format(intend*2, current_path, file_))
+                displayPath('{}\\{}'.format(current_path, file_), intend=intend*2)
+
+def convertPathToTree(current_path) :
+    pass
